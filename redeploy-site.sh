ac76a2e1c21540da315054ee99dc19129f94612b
tapaswini-kodavanti <58580071+tapaswini-kodavanti@users.noreply.github.com>
2023-07-10 15:38:43 -0500

    using tmux send keys

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,6 @@
 tmux kill-session
 git fetch && git reset origin/main --hard
-tmux new
-tmux send-keys "source python3-virtualenv/bin/activate" Enter
-tmux send-keys "pip install -r requirements.txt" Enter
-tmux send-keys "flask run --host=0.0.0.0" Enter
+tmux new-session -s my_session -d
+tmux send-keys -t my_session "source python3-virtualenv/bin/activate" Enter
+tmux send-keys -t my_session "pip install -r requirements.txt" Enter
+tmux send-keys -t my_session "flask run --host=0.0.0.0" Enter
