bf7615d7327e98022e6a475b65f5bdae44a6bd6c
tapaswini-kodavanti <58580071+tapaswini-kodavanti@users.noreply.github.com>
2023-07-15 18:42:19 -0500

    created bash script for testing GET, POST, and DELETE endpoints

--- curl-test.sh
+++ curl-test.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-curl --request http://localhost:5000/api/time
+curl --request POST http://localhost:5000/api/timeline_post -d 'name=tester&email=tester@gmail.com&content=testing'
+info=$(curl -s -X GET http://localhost:5000/api/timeline_post)
+timeline_post=$(echo $info | jq '.timeline_posts')
+post=$(echo $timeline_post | jq '.[0]')
+
+name=$(echo "$post" | jq -r '.name')
+email=$(echo "$post" | jq -r '.email')
+content=$(echo "$post" | jq -r '.content')
+
+
+echo $name
+echo $email
+echo $content
+
+if [ "$name" != "tester" ] || [ "$email" != "tester@gmail.com" ] || [ "$content" != "testing" ]; then
+    echo "Error: GET or POST endpoint failed" >&2
+    exit 1
+else
+    echo "GET and POST working correctly"
+fi
+
+id=$(echo $post | jq '.id')
+curl --request DELETE http://localhost:5000/api/timeline_post/$id
