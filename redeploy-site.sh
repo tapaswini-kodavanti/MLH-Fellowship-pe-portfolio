72c48f1b78244bc71b80349e38e856e02d130c22
tapaswini-kodavanti <58580071+tapaswini-kodavanti@users.noreply.github.com>
2023-07-30 18:45:32 -0500

    updated redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,4 @@
 cd MLH-Fellowship-pe-portfolio
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --buildß
