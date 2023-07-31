91be2f9b800ed1ad68430c5d47be7f5b8b69cac3
tapaswini-kodavanti <58580071+tapaswini-kodavanti@users.noreply.github.com>
2023-07-30 20:05:09 -0500

    fixed typo

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,4 +1,4 @@
 cd MLH-Fellowship-pe-portfolio
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml up -d --buildß
+docker compose -f docker-compose.prod.yml up -d --build
